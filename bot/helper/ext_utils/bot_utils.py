57bad10b1b4af15a6d95e5fb4ccd442f4735d520
tharindupraba <64023491+tharindupraba@users.noreply.github.com>
2022-09-25 03:24:58 +0530

    Update bot_utils.py
    Signed-off-by: tharindupraba <64023491+tharindupraba@users.noreply.github.com>

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -240,9 +240,13 @@ def get_readable_time(seconds: int) -> str:
 def is_url(url: str):
     url = re_findall(URL_REGEX, url)
     return bool(url)
-
+    
 def is_gdrive_link(url: str):
     return "drive.google.com" in url
+
+def is_gdtot_link(url: str):
+    url = re_match(r'https?://.+\.gdtot\.\S+', url)
+    return bool(url)
 
 def is_mega_link(url: str):
     return "mega.nz" in url or "mega.co.nz" in url
