8c9933dabb95a04d7050be3e37569d073c9963c9
tharindupraba <64023491+tharindupraba@users.noreply.github.com>
2022-10-11 21:42:49 +0530

    Update bot_utils.py
    Signed-off-by: tharindupraba <64023491+tharindupraba@users.noreply.github.com>

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -228,9 +228,9 @@ def is_gdrive_link(url: str):
     return "drive.google.com" in url
 
 def is_gdtot_link(url: str):
-    #url = re_match(r'https?://.+\.gdtot\.\S+', url)
-    #return bool(url)
-     return "new.gdtot.re" in url
+     url = re_match(r'https?://.+\.gdtot\.\S+', url)
+     return bool(url)
+     #return "new.gdtot.re" in url
 def is_mega_link(url: str):
     return "mega.nz" in url or "mega.co.nz" in url
 
